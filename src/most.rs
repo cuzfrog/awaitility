e834aa27ffbb4b246bed8e76f46b2dff8edcf83f
xiangbin li <dassio@icloud.com>
2023-08-14 22:37:54 +0800

    add until_no_panic  (#4)

--- src/most.rs
+++ src/most.rs
@@ -1,6 +1,7 @@
 use crate::error::Timeout;
 use super::backend::Backend;
 use std::time::{Duration, Instant};
+use std::panic::{AssertUnwindSafe, self};
 
 pub struct MostWait<'a> {
     duration: Duration,
@@ -49,12 +50,28 @@ impl<'a> MostWait<'a> {
         }
         self
     }
+
+    pub fn until_no_panic(&mut self, f: impl Fn()) -> &Self {
+        let now = Instant::now();
+        let mut result = panic::catch_unwind(AssertUnwindSafe(|| f()));
+        while result.is_err() {
+            result = panic::catch_unwind(AssertUnwindSafe(|| f()));
+            let elapsed = now.elapsed();
+            if elapsed > self.duration {
+                f();
+                break;
+            }
+            std::thread::sleep(self.backend.interval);
+        }
+        self
+    }
 }
 
 #[cfg(test)]
 mod most_test {
     use std::sync::atomic::{AtomicUsize, Ordering};
     use std::sync::Arc;
+    use std::{thread, time};
     use std::time::Duration;
 
     #[test]
@@ -67,6 +84,21 @@ mod most_test {
             }
         });
         super::at_most(Duration::from_millis(100)).until(|| counter.load(Ordering::SeqCst) > 10);
+    }
+
+    #[test]
+    #[should_panic]
+    fn at_most_using_assert() {
+        let counter = Arc::new(AtomicUsize::new(5));
+        let tcounter = counter.clone();
+        std::thread::spawn(move || {
+            while tcounter.load(Ordering::SeqCst) < 15 {
+                let ten_millis = time::Duration::from_millis(10);
+                thread::sleep(ten_millis);
+                tcounter.fetch_add(1, Ordering::SeqCst);
+            }
+        });
+        super::at_most(Duration::from_millis(1000)).until_no_panic(|| assert!(counter.load(Ordering::SeqCst) < 3));
     }
 
     #[test]
